eee3df87dcc6c9c6664bb15b2ac6e13caba820db
Henrik Braune <hb@braune-digital.com>
2015-01-27 22:23:01 +0100

    Added slug

--- Entity/City.php
+++ Entity/City.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\Table;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * City
@@ -28,6 +30,12 @@ class City extends Locality
      * @var State
      */
     protected $state;
+
+    /**
+     * @Gedmo\Slug(fields={"nameAscii"})
+     * @ORM\Column(length=128, unique=true)
+     */
+    private $slug;
 
     /**
      * Returns the state
@@ -58,4 +66,22 @@ class City extends Locality
     public function getGeopoint() {
         return $this->getLatitude() . ',' . $this->getLongitude();
     }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+
 }
